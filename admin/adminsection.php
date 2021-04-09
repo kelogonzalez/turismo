7eeec6dea201118e5b1f30872b7fe1144553c65f
kelogonzalez <kelo.gonzalez@gmail.com>
2021-04-09 10:24:32 -0500

    Actualizacion del modulo de Seccion Turismo

--- admin/adminsection.php
+++ admin/adminsection.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Secciones</title>
+    <title>Tipo de Turismo</title>
     <?php
         session_start();
         $LinksRoute="../";
-        include '../inc/links.php'; 
+        include '../inc/links.php';
     ?>
     <script src="../js/SendForm.js"></script>
 </head>
 <body>
-    <?php 
+    <?php
         include '../library/configServer.php';
         include '../library/consulSQL.php';
         include '../process/SecurityAdmin.php';
         include '../inc/NavLateral.php';
     ?>
     <div class="content-page-container full-reset custom-scroll-containers">
-        <?php 
+        <?php
             include '../inc/NavUserInfo.php';
         ?>
         <div class="container">
             <div class="page-header">
-              <h1 class="all-tittles">Sistema bibliotecario <small>Administración Institución</small></h1>
+              <h1 class="all-tittles">Sistema Turístico <small>Administración Institución</small></h1>
             </div>
         </div>
         <div class="container-fluid">
             <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
               <li role="presentation"><a href="admininstitution.php">Institución</a></li>
-              <li role="presentation"><a href="adminprovider.php">Proveedores</a></li>
-              <li role="presentation"><a href="admincategory.php">Categorías</a></li>
-              <li role="presentation"  class="active"><a href="adminsection.php">Secciones</a></li>
+              <li role="presentation"><a href="adminprovider.php">Departamentos</a></li>
+              <li role="presentation"><a href="admincategory.php">Actividades</a></li>
+              <li role="presentation"  class="active"><a href="adminsection.php">Tipo de Turismo</a></li>
             </ul>
         </div>
         <div class="container-fluid"  style="margin: 50px 0;">
@@ -39,7 +39,7 @@
                     <img src="../assets/img/section.png" alt="user" class="img-responsive center-box" style="max-width: 110px;">
                 </div>
                 <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
-                    Bienvenido a la sección para registrar nuevas secciones al sistema, debes de seleccionar los datos en el siguiente formulario para registrar una sección
+                    Bienvenido a la sección para registrar nuevos tipos de turismo al sistema, debes de seleccionar los datos en el siguiente formulario para registrar una sección
                 </div>
             </div>
         </div>
@@ -47,29 +47,29 @@
             <div class="row">
                 <div class="col-xs-12 lead">
                     <ol class="breadcrumb">
-                      <li class="active">Nueva sección</li>
-                      <li><a href="adminlistsection.php">Listado de secciones</a></li>
+                      <li class="active">Nuevo tipo de turismo</li>
+                      <li><a href="adminlistsection.php">Listado de tipos de turismo</a></li>
                     </ol>
                 </div>
             </div>
         </div>
         <div class="container-fluid">
             <div class="container-flat-form">
-                <div class="title-flat-form title-flat-blue">Agregar una nueva sección</div>
+                <div class="title-flat-form title-flat-blue">Agregar un nuevo tipo de turismo</div>
                 <form action="../process/AddSection.php" method="post" class="form_SRCB" data-type-form="save" autocomplete="off">
                     <div class="row">
                         <div class="col-xs-12 col-sm-8 col-sm-offset-2">
                             <div class="group-material">
-                                <input type="text" class="material-control tooltips-general" placeholder="Escribe aquí el grado" name="sectionGrade" required="" maxlength="1" pattern="[1-9]{1,1}" data-toggle="tooltip" data-placement="top" title="Solo números del 1 al 9">
+                                <input type="text" class="material-control tooltips-general" placeholder="Escribe aquí la clasificación" name="sectionGrade" required="" maxlength="13" pattern="[0-9]{1,13}" data-toggle="tooltip" data-placement="top" title="Solo números del 1 al 9">
                                 <span class="highlight"></span>
                                 <span class="bar"></span>
-                                <label>Año/Grado</label>
+                                <label>Clasificación MINTUR</label>
                             </div>
                             <div class="group-material">
-                                <input type="text" class="material-control tooltips-general" placeholder="Escribe aquí la especialidad" name="sectionSpecialty" required="" maxlength="31" pattern="[a-zA-ZáéíóúÁÉÍÓÚ() ]{5,31}" data-toggle="tooltip" data-placement="top" title="Por ejemplo: Informática, Enfermería, Grado etc...">
+                                <input type="text" class="material-control tooltips-general" placeholder="Escribe aquí la categoría" name="sectionSpecialty" required="" maxlength="31" pattern="[a-zA-ZáéíóúÁÉÍÓÚ() ]{5,31}" data-toggle="tooltip" data-placement="top" title="Por ejemplo: Informática, Enfermería, Hospedaje etc...">
                                 <span class="highlight"></span>
                                 <span class="bar"></span>
-                                <label>Especialidad/Nivel de Grado</label>
+                                <label>Categoría</label>
                             </div>
                             <div class="group-material">
                                 <input type="text" class="material-control tooltips-general" placeholder="Escribe aquí la sección" name="sectionSection" required="" maxlength="1" pattern="[a-zA-ZáéíóúÁÉÍÓÚ]{1,1}" data-toggle="tooltip" data-placement="top" title="Por ejemplo: A, B, C, D etc...">
@@ -80,7 +80,7 @@
                             <p class="text-center">
                                 <button type="reset" class="btn btn-info" style="margin-right: 20px;"><i class="zmdi zmdi-roller"></i> &nbsp;&nbsp; Limpiar</button>
                                 <button type="submit" class="btn btn-primary"><i class="zmdi zmdi-floppy"></i> &nbsp;&nbsp; Guardar</button>
-                            </p> 
+                            </p>
                        </div>
                    </div>
                 </form>
